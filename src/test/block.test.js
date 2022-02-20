38b9e69d911cedb3eda78558755ccd6ffcbd24ff
ehung <ehung@synology.com>
2022-02-20 19:03:47 +0800

    test Block::validate

--- src/test/block.test.js
+++ src/test/block.test.js
@@ -1,7 +1,26 @@
+const { Block } = require("../block.js");
+
+const createData = ({ data }) => ({ data });
+const createBlock = ({ data = createData("Genesis Block") } = {}) =>
+  new Block(data);
+
 describe("block", () => {
   describe("validate", () => {
-    it.todo("should be valid");
-    it.todo("should be invalid");
+    // FIXME: test after implementation
+    it.skip("should be valid if block has not been tampered", async () => {
+      const block = createBlock();
+      const isValid = await block.validate();
+      expect(isValid).toBeTruthy();
+    });
+
+    // FIXME: test after implementation
+    it.skip("should be invalid if block is tampered", async () => {
+      const block = createBlock();
+      const fakeData = createData({ data: "vicious data" });
+      block.data = Buffer.from(JSON.stringify(fakeData)).toString("hex");
+      const isValid = await block.validate();
+      expect(isValid).toBeFalsy();
+    });
   });
 
   describe("getBData", () => {
